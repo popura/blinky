29827d5fdbf6db05dd78a20a82d1ce8c82934653
popura <ykinoshita@tmu.ac.jp>
2020-07-20 17:46:46 +0900

    some fixed

--- firmware/blinky_ota/firmware/main/otohikari.cpp
+++ firmware/blinky_ota/firmware/main/otohikari.cpp
@@ -30,7 +30,8 @@ enum class State : char
   RED_BLUE_DOUBLE_REF = 4,
   RED_CALIBRATION = 5,
   WHITE_CALIBRATION = 6,
-  STATE_7 = 7
+  //STATE_7 = 7
+  DOA = 7
 };
 State state_current = State::WHITE_SIGNAL;
 
@@ -40,6 +41,9 @@ int current_led = LED_WHITE;
 
 // DC removal filter
 DCRemoval dc_rm(DC_REMOVAL_ALPHA);
+
+// Distance between microphones [m]
+float D_MICS = 0.030
 
 void main_process()
 {
@@ -266,9 +270,90 @@ void main_process()
             }
             break;
 
-          case State::STATE_7:
-            state_current = state_new;
-            vTaskDelay(10 / portTICK_PERIOD_MS);
+        //case State::STATE_7:
+        //    state_current = state_new;
+        //    vTaskDelay(10 / portTICK_PERIOD_MS);
+        //    break;
+        case State::DOA:
+            {
+              if (state_new != state_current)
+              {
+                state_current = state_new;
+
+                // Turn off all LEDs
+                for (int i = 0 ; i < leds.size() ; i++)
+                  ledC->updateDuty(leds[i], 0);
+
+                counter = 0;
+              }
+
+              timer->start();
+              // Two channels processing
+              for (int n=0; n<2; n++) {
+
+                float filter_output = 0.;
+                for (int i = 0 ; i < AUDIO_BUFFER_SIZE ; i++) {
+                  // remove the mean using a notch filter
+                  float sample = dc_rm.process(audio_data[AUDIO_CHANNELS * i + n]);
+                  // square to compute the power and accumulate
+                  filter_output += sample * sample;
+                }
+                // divide to obtain mean power in the current buffer
+                filter_output /= AUDIO_BUFFER_SIZE;
+
+                // Apply the non-linear transformation
+                float val_db = decibels(filter_output);
+                float duty_f = map_to_unit_interval(val_db, MIN_DB, MAX_DB);
+                duty_f = blinky_non_linearity(duty_f);  // this will also clip in [0, 1]
+
+                // Detect if the signal is too large
+                if (n == 0)
+                {
+                  if (duty_f >= 1.)
+                    ledC->updateDuty(leds[LED_GREEN], 400);
+                  else
+                    ledC->updateDuty(leds[LED_GREEN], 0);
+                }
+                else if (n == 1)
+                {
+                  if (duty_f >= 1.)
+                    ledC->updateDuty(leds[LED_BLUE], 400);
+                  else
+                    ledC->updateDuty(leds[LED_BLUE], 0);
+                }
+
+                // Set the LED duty cycle
+                uint32_t duty = 0;
+                if (n == 0)
+                {
+                  duty = (uint32_t)(duty_f * duty_max[LED_RED]);
+                  ledC->updateDuty(LED_RED, duty);
+                }
+                else if (n == 1)
+                {
+                  duty = (uint32_t)(duty_f * duty_max[LED_WHITE]);
+                  ledC->updateDuty(LED_WHITE, duty);
+                }
+
+                if (ENABLE_MONITOR and counter % 50 == 0)
+                {
+                  printf("power_val=%e db=%d duty_f=%e duty=%d dip_val=%d\n",
+                      (double)filter_output, (int)val_db, (double)duty_f, (int)duty, (int)dip_switch.read());
+                }
+                counter += 1;
+              }
+
+              elapsed_time = timer->measure();
+              if (elapsed_time > (float)AUDIO_BUFFER_SIZE/(float)SAMPLE_RATE*1000.0f)
+              {
+                // elapsed_time must be less than AUDIO_BUFFER_SIZE/SAMPLE_RATE*1000(msec)
+                printf(
+                    "elapsed_time must be less than %f(msec)\n",
+                    (float)AUDIO_BUFFER_SIZE/(float)SAMPLE_RATE*1000.0f
+                );
+              }
+
+            }
             break;
         }
     }
